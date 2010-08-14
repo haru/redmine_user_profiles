f19c6039c6ca49ec2f9c47f0a8b8b141d601582e
haru_iida <devnull@localhost>
2010-08-14 14:39:41 +0900

    initial commit

---
+++ init.rb
@@ -0,0 +1,33 @@
+# User Profiles plugin for Redmine
+# Copyright (C) 2010  Haruyuki Iida
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+require 'redmine'
+require 'user_profiles_my_accout_hooks'
+require 'user_profiles_user_patch'
+require 'user_profiles_users_hooks'
+
+Redmine::Plugin.register :redmine_user_profiles do
+  name 'Redmine User Profiles plugin'
+  author 'Haruyuki Iida'
+  description 'This is a plugin for Redmine lets each user edit a profile.'
+  version '0.0.1'
+  url 'http://www.r-labs.org/projects/show/userprofile'
+  author_url 'http://twitter.com/haru_iida'
+  requires_redmine :version_or_higher => '1.0.0'
+
+end
---
+++ test/unit/user_profile_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class UserProfileTest < ActiveSupport::TestCase
+  fixtures :user_profiles
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
